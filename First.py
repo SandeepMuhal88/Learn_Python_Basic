6e0125ba2a10e2560886e2627e0461c00bac47b1
Sandeep Muhal <sandeepmuhal9982@gamil.com>
2024-09-12 06:38:59 +0530

    Learn python

---
+++ First.py
@@ -0,0 +1,14 @@
+print("Sandeep Muhal")
+print("Perfrom operation:-)))) ")
+Name="Sandeep"
+c=153.25
+a=25 
+b=5
+print("Sum=", a+b)
+print("Division=", a/b)
+print("Substracation=", a-b)
+print("Multiplication=", a*b)
+print("Module=", a%b)
+print(type(Name))
+print(type(a))
+print(type(c))

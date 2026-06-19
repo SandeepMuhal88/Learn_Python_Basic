73c1c5c32cd162852a1047eb1f38c778d2d7010f
SandeepMuhal88 <sandeepmuhal9982@gmail.com>
2026-06-19 22:22:04 +0530

    Update code

--- list_Data_Structure/list_5.py
+++ list_Data_Structure/list_5.py
@@ -6,3 +6,12 @@ k = 2
 k = k % len(arr)  # Handle cases where k is greater than the length of the list
 rotated_arr = arr[-k:] + arr[:-k]
 print("The rotated list is: ", rotated_arr)
+
+# Find Missing Number
+
+arr = [1,2,4,5]
+
+all_numbers_sum = sum(range(1, len(arr) + 2))  # Sum of first n natural numbers
+arr_sum = sum(arr)
+missing_number = all_numbers_sum - arr_sum
+print("The missing number is: ", missing_number)
